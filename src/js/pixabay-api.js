849399a2f651e09201533f69e6e2c4f39fe96f76
NataliiaBumbu <130250304+NataliiaBumbu@users.noreply.github.com>
2024-08-01 13:33:32 +0300

    hw12

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,33 +1,47 @@
-import { MESSAGES, MESSAGES_BG_COLORS, showInfoMessage} from "./messages";
+import axios from 'axios';
+import {MESSAGES, MESSAGES_BG_COLORS, showInfoMessage  } from './messages';
+import { fetchLoader } from './render-functions';
 
+const API_KEY = '45125188-29656ded7c12a384255e46c86';
+const API_URL = 'https://pixabay.com/api/?';
+const CONFIG = {
+  params: {
+    key: API_KEY,
+    image_type: 'photo',
+    orientations: 'horizontal',
+    safesearch: true,
+    page: 1,
+    per_page: 15,
+  },
+};
 
-const options = {
-    method: 'GET',
-  };
-  const API_URL = 'https://pixabay.com/api/?';
-  const API_KEY = '45125188-29656ded7c12a384255e46c86';
-  
-  export async function getGalleryData(queryValue) {
-    try {
-      const searchParams = new URLSearchParams({
-        key: API_KEY,
-        q: queryValue,
-        image_type: 'photo',
-        orientation: 'horizontal',
-        safesearch: true,
-      });
-  
-      const response = await fetch(API_URL + searchParams, options).then();
-      if (!response.ok) {
-        showInfoMessage(MESSAGES.error, MESSAGES_BG_COLORS.orange);
-        return;
-      }
-      return await response.json();
-    } catch (err) {
+export async function getGalleryData(queryValue, page) {
+  try {
+    fetchLoader();
+    CONFIG.params.q = queryValue;
+    CONFIG.params.page = page;
+    const response = await axios.get(API_URL, CONFIG);
+    return response.data;
+  } catch (error) {
+    if (error.response) {
+      const { data } = error.response;
+    
       showInfoMessage(
-        `${MESSAGES.exception} ERROR:  ${err}`,
+        `${MESSAGES.exception} ERROR: ${data}`,
+        MESSAGES_BG_COLORS.orange
+      );
+    } else if (error.request) {
+      
+      showInfoMessage(
+        `${MESSAGES.exception} ERROR: ${error.request}`,
+        MESSAGES_BG_COLORS.orange
+      );
+    } else {
+      
+      showInfoMessage(
+        `${MESSAGES.exception} ERROR: ${error.message}`,
         MESSAGES_BG_COLORS.orange
       );
     }
   }
-
+}
--- src/js/render-functions.js
+++ src/js/render-functions.js
@@ -4,14 +4,15 @@ const lightbox = new SimpleLightbox('.gallery a', {
   captionsData: 'alt',
   captionDelay: 250,
 });
+const gallery = document.querySelector('.gallery');
 
-export function renderGallery(data, tagToInsert) {
-  tagToInsert.innerHTML = markup(data);
+export function fetchGallery(data) {
+  gallery.insertAdjacentHTML('beforeend', markupGallery(data));
 
   lightbox.refresh();
 }
 
-function markup(data) {
+function markupGallery(data) {
   return data.hits
     .map(
       ({
@@ -22,21 +23,32 @@ function markup(data) {
         views,
         comments,
         downloads,
-      }) => `<li class="gallery-item hvr-grow">
-	 <a class="gallery-link" href="${largeImageURL}">
-	 <figure class="gallery-figure ">
-	 <img class="gallery-image" src="${webformatURL}" alt="${tags}" loading="lazy">
-	 <figcaption class="gallery-figcaption">
-	  <ul class="img-content-wrapper">
-		<li>Likes<span>${likes}</span></li>
-		<li>Views<span>${views}</span></li>
-		<li>Comments<span>${comments}</span></li>
-		<li>Downloads<span>${downloads}</span></li>
-	   </ul>
-	 </figcaption>
-	 </figure>
-	 </a>
-	 </li>`
+      }) => `
+				<li class="gallery-item hvr-grow">
+					<a class="gallery-link" href="${largeImageURL}">
+						<figure class="gallery-figure ">
+						<img class="gallery-image" src="${webformatURL}" alt="${tags}" loading="lazy">
+						<figcaption class="gallery-figcaption">
+						<ul class="img-content-wrapper">
+									<li>Likes<span>${likes}</span></li>
+									<li>Views<span>${views}</span></li>
+									<li>Comments<span>${comments}</span></li>
+									<li>Downloads<span>${downloads}</span></li>
+						</ul>
+						</figcaption>
+						</figure>
+					</a>
+				</li>
+		`
     )
     .join('');
+}
+
+export function fetchLoader() {
+  gallery.insertAdjacentHTML(
+    'beforeend',
+    `<div class='loader-wrapper'>
+        <div class='loader'></div>
+    </div>`
+  );
 }
